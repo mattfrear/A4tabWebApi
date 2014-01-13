32b83e9d563b66e03b5e6529800aa7d4718c0628
Matt Frear <matt.frear@gmail.com>
2014-01-13 21:51:02 +0000

    Fleshed out spec. Added first controller and unit test

---
+++ A4tabWebApi.UnitTests/Controllers/TabsControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using A4tabWebApi.Controllers;
+using Should;
+using NUnit.Framework;
+
+namespace A4tabWebApi.UnitTests.Controllers
+{
+    [TestFixture]
+    public class TabsControllerTests
+    {
+        public class Get
+        {
+            [Test]
+            public void Should_Return_List_Of_Tabs()
+            {
+                // Arrange
+                var controller = new TabsController();
+
+                // Act
+                var result = controller.Get();
+                
+                // Assert
+                result.ShouldNotBeEmpty();
+                result.Count().ShouldEqual(2);
+                result.ToList()[0].ShouldEqual(1);
+            }
+        }
+    }
+}
---
+++ A4tabWebApi/Controllers/TabsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace A4tabWebApi.Controllers
+{
+    public class TabsController : ApiController
+    {
+        // GET api/tabs
+        public IEnumerable<int> Get()
+        {
+            return new [] { 1, 2 };
+        }
+
+        // GET api/tabs/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/tabs
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/tabs/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/tabs/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
