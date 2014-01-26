4e05b1cdf1aca1bbed2102996bb2bf6f98284c13
Matt Frear <matt.frear@gmail.com>
2014-01-26 16:07:02 +0000

    Wired up home page to api using angular

---
+++ Web/app/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("a4tab", []);
+
+app.controller("RecentCtrl", function ($scope, $http) {
+    $http.get("http://localhost:1120/api/tabs").success(function(data) {
+        $scope.recentTabs = data;
+    }).error(function() {
+        $scope.error = "Couldn't load Recent tabs.";
+    });
+});
