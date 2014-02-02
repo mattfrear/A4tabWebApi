5996d8d4033c790f75e8311608a1809605eb34ce
Matt Frear <matt.frear@gmail.com>
2014-02-02 17:59:49 +0000

    Added Angular routing

--- Web/app/app.js
+++ Web/app/app.js
@@ -1,9 +1,37 @@
-﻿var app = angular.module("a4tab", []);
+﻿var app = angular.module("a4tab", ['ngRoute']);
 
-app.controller("RecentCtrl", function ($scope, $http) {
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'recentCtrl'
+        })
+        .when('/tabs/', {
+            templateUrl: 'pages/tabs.html',
+            controller: 'tabsCtrl'
+        })
+        .when('/tabs/:id', {
+            templateUrl: 'pages/tab.html',
+            controller: 'tabCtrl'
+        });
+});
+
+app.controller("recentCtrl", function ($scope, $http) {
     $http.get("http://localhost:1120/api/v1/tabs/recent").success(function(data) {
         $scope.recentTabs = data;
     }).error(function() {
         $scope.error = "Couldn't load Recent tabs.";
     });
+});
+
+app.controller("tabsCtrl", function($scope, $http) {
+    $http.get("http://localhost:1120/api/v1/tabs/").success(function (data) {
+        $scope.tabs = data;
+    }).error(function () {
+        $scope.error = "Couldn't load tabs.";
+    });
+});
+
+app.controller("tabCtrl", function($scope, $http, $routeParams) {
+    $scope.message = $routeParams.id;
 });
