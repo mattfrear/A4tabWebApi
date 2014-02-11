0523c1b84e748935be2dd61aa6ebdbb4233b71ef
Matt Frear <matt.frear@gmail.com>
2014-02-11 21:56:08 +0000

    Upgraded packages

--- A4tabWebApi/Scripts/_references.js
+++ A4tabWebApi/Scripts/_references.js
@@ -1,5 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="modernizr-2.7.1.js" />
-/// <reference path="bootstrap.js" />
 /// <reference path="respond.js" />
 /// <reference path="jquery-2.1.0.js" />
+/// <reference path="bootstrap.js" />
