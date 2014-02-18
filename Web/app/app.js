1114b38893b6768fbc356c24e8e8492cc0fb4f3e
Matt Frear <matt.frear@gmail.com>
2014-02-18 22:23:22 +0000

    Changed page titles

--- Web/app/app.js
+++ Web/app/app.js
@@ -4,17 +4,25 @@ app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'pages/home.html',
-            controller: 'recentCtrl'
+            controller: 'recentCtrl',
+            title: 'Home'
         })
         .when('/tabs/', {
             templateUrl: 'pages/tabs.html',
-            controller: 'tabsCtrl'
+            controller: 'tabsCtrl',
+            title: 'All tabs'
         })
         .when('/tabs/:id', {
             templateUrl: 'pages/tab.html',
             controller: 'tabCtrl'
         });
 });
+
+app.run(['$location', '$rootScope', function ($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
 
 app.controller("recentCtrl", function ($scope, $http) {
     $http.get("http://localhost:1120/api/v1/tabs/recent").success(function(data) {
@@ -32,10 +40,11 @@ app.controller("tabsCtrl", function($scope, $http) {
     });
 });
 
-app.controller("tabCtrl", function($scope, $http, $routeParams) {
+app.controller("tabCtrl", function($scope, $http, $routeParams, $rootScope) {
     
     $http.get("http://localhost:1120/api/v1/tabs/" + $routeParams.id).success(function (data) {
         $scope.tab = data;
+        $rootScope.title = data.Name;
     }).error(function () {
         $scope.error = "Couldn't load tabs.";
     });
